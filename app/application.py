1c7bb04a26cc3355f13e1e3e07a86a9dad3960e4
Benjamin Congdon <bencongdon96@gmail.com>
2017-03-07 16:29:20 -0600

    DB settings

--- app/application.py
+++ app/application.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import pymysql
+from settings import MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_PORT, MYSQL_DB
 
 app = Flask(__name__)
+
+conn = pymysql.connect(host=MYSQL_HOST,
+                       user=MYSQL_USER,
+                       password=MYSQL_PASS,
+                       port=MYSQL_PORT,
+                       db=MYSQL_DB,
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 
 @app.route('/')
--- app/settings.py
+++ app/settings.py
@@ -8,4 +8,5 @@ load_dotenv(dotenv_path)
 MYSQL_USER = os.environ['MYSQL_USER']
 MYSQL_PASS = os.environ['MYSQL_PASS']
 MYSQL_HOST = os.environ['MYSQL_HOST']
-MYSQL_HOST = os.environ.get('MYSQL_PORT', 3306)
+MYSQL_PORT = os.environ.get('MYSQL_PORT', 3306)
+MYSQL_DB = os.environ.get('MYSQL_DB', 'DropMuse')
