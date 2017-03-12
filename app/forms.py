17b87afdcfc46975caaf5a33f7914fd9ed460ea2
Benjamin Congdon <bencongdon96@gmail.com>
2017-03-12 08:58:53 -0500

    Added favicons, email validation

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,17 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, validators
+from wtforms.fields.html5 import EmailField
 
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    username = StringField('Username', [
+        validators.DataRequired(),
+        validators.Length(min=4, max=25)
+    ])
+    email = EmailField('Email Address', [
+        validators.Length(min=4, max=30),
+        validators.DataRequired(),
+        validators.Email()
+    ])
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
