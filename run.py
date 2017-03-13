a46f4c4d1bfd785404c177399961e75f9fddc883
Benjamin Congdon <bencongdon96@gmail.com>
2017-03-13 17:41:08 -0500

    Fix deploy port

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
 from app.application import app
 import os
-PORT = os.environ.get('port', 5000)
+PORT = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=PORT)
