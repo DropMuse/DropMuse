a5b4a3bdcf3a65f62b7a3125b2228e0781daee16
Benjamin Congdon <bencongdon96@gmail.com>
2017-03-15 07:30:46 -0500

    Revert run.py

--- run.py
+++ run.py
@@ -2,6 +2,5 @@
 from app.application import app
 import os
 
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=PORT)
+PORT = int(os.environ.get('PORT', 5000))
+app.run(debug=True, port=PORT)
