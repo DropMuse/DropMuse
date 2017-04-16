9c168b2ee9ae2dd58fd938d68e68ba083e5d2770
Benjamin Congdon <bencongdon96@gmail.com>
2017-04-16 18:53:13 -0500

    Fix to duration format

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,8 @@ def format_duration(seconds):
         return "0:00"
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
-    formatted = "%d:%02d" % (m, s)
+    fmt = "%02d:%02d" if h > 0 else "%d:%02d"
+    formatted = fmt % (m, s)
     if h:
         formatted = ("%d:" % h) + formatted
     return formatted
